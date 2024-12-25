1af8ffc2fa6b8e3498fd7a5c2bb9e206742114ec
BastosDev <gustavoporto.gp63@gmail.com>
2024-12-25 15:32:45 -0300

    add new exemples

--- arquivo.js
+++ arquivo.js
@@ -1,7 +1,7 @@
 let numero = 300
 const numero2 = 20;
-
 const nome = "felps";
+const estaChovendo = true
 
 console.log(`Oi, ${nome}`)
 
@@ -11,3 +11,5 @@ console.log(numero2)
 numero = 100
 
 console.log(numero)
+
+console.log(`Condição do clima é chuvoso?\n${estaChovendo}`)
---
+++ arquivo2.js
@@ -0,0 +1,8 @@
+const number1 = 10
+const number2 = 20
+
+let soma
+
+soma = number1 + number2
+
+console.log(soma)
